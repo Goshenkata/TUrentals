0d10b0e11f53870e4c969fbbeb8b1bd7b5e71f7c
Goshenkata <goshenkataklev@gmail.com>
2024-12-02 20:32:27 +0200

    fixed annotation

--- api/src/main/java/com/example/demo/controller/OrderController.java
+++ api/src/main/java/com/example/demo/controller/OrderController.java
@@ -8,18 +8,20 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.security.Principal;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 @RequestMapping("order/")
 public class OrderController {
     private final OrderService orderService;
 
-    @RequestMapping("create")
+    @PostMapping("create")
     public ResponseEntity<MessageResponseDTO> createOrder(@Valid @RequestBody OrderCreateDTO orderCreateDTO, BindingResult bindingResult, Principal principal) {
         if (bindingResult.hasErrors()) {
             return ResponseEntity.badRequest().build();
